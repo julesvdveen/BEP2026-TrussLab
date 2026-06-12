79781c6ce2bdc8d565eb13832babbc99d8afaf4d
Rein-sys <rvholk@gmail.com>
2026-06-12 11:42:46 +0200

    myDAQ_changes_docs

--- TrussLab/myDAQ_manager.py
+++ TrussLab/myDAQ_manager.py
@@ -1,3 +1,7 @@
+# The myDAQ_manager is able to control the myDAQ. The python file controls labVIEW and labVIEW controls the myDAQ.
+# The code below opens a labVIEW file on your laptop and runs it.
+# It is important for the user to have all the folders on your laptop sequenced the same way as in Python.
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -5,19 +9,15 @@ import time
 from datetime import datetime
 from scipy.ndimage import median_filter  
 
-# filename = "resultaten1.txt"
-# f_start = np.array([1000])
-# f_stop = np.array([20000])
-# steps = np.array([1500])
 VI_PATH = str(Path(__file__).parent / "temp" / "Mydaq12.vi")
-# VI_PATH = r"C:\Users\Reinoud van Holk\OneDrive - Delft University of Technology\Bestanden van Jules van der Veen - BEP 2026\Python\U-U Filter\temp\Mydaq12.vi"
-#" C:\Data\aansturen mydaq\Mydaq123.vi"
 
 def logs_to_U(filename_log:str, U_in:float=1, Cartesian=True):
+    """
+    Converts myDAQ Bode log to frequency and complex voltage arrays. 
+    This code does not run labVIEW, so you will have to sweep the frequencies
+    manually using the 'National Instruments' app.
+    """
 
-    """
-    Converts myDAQ Bode log to frequency and complex voltage arrays.
-    """
     df = pd.read_csv(filename_log, skiprows=3, sep='\s+', decimal=',', 
                  names=['freq', 'gain_db', 'phase_deg'])
     
@@ -35,26 +35,34 @@ def logs_to_U(filename_log:str, U_in:float=1, Cartesian=True):
         return f_arr, U
 
 def myDAQ_sweep(f_start_list:list, f_stop_list:list, steps:list, filename:str, Cartesian:bool=True):
-    print("myDAQsweep started...")
-    Path("temp").mkdir(exist_ok=True)
-    import win32com.client
-
     """
     Function to operate the myDAQ manager from Python, starts a labview file to sweep the bridge.
 
-    Args:
+    Arguments:
     f_start (list): starting freq
     f_stop (list): stopping freq
     steps (list): steps/decade
-    """
 
+    Explanation:
+    This function is capable to start and run a labVIEW file. 
+    This function is capable of doing the information dense frequency sweeping, 
+        because it deletes the previous 'test' txt-files. 
+    This function has a loop of a time delay, 
+        because the code needs to wait for the myDAQ till it is done sweeping.
+    When labVIEW is done sweeping, it copies the values to a list. The list is converted to a txt-file.
+    """
+    import win32com.client # Only compatible with Windows
+
+    print("myDAQsweep started...")
+    Path("temp").mkdir(exist_ok=True)
+    
     if len(f_start_list) != len(f_stop_list) or len(f_start_list) != len(steps):
         raise Exception("The start, stop and steps lists do not have matching dimensions.")
     
     labview = win32com.client.Dispatch("LabVIEW.Application")
     vi = labview.GetVIReference(VI_PATH)
 
-    # Belangrijk: pywin32 moet weten dat dit methodes zijn
+    # Important: pywin32 needs to know that these are methods:
     vi._FlagAsMethod("SetControlValue")
     vi._FlagAsMethod("Run")
 
@@ -75,7 +83,7 @@ def myDAQ_sweep(f_start_list:list, f_stop_list:list, steps:list, filename:str, C
         vi.SetControlValue("stop_frequency_Hz", float(stop_frequency_Hz))
         vi.SetControlValue("steps_per_decade", int(steps_per_decade))
 
-        vi.Run(True)
+        vi.Run(True) # This runs the labVIEW file 
         lvm = Path(__file__).parent / "temp" / "test.lvm"
 
         # Record the starting time of the loop
@@ -91,8 +99,7 @@ def myDAQ_sweep(f_start_list:list, f_stop_list:list, steps:list, filename:str, C
             
             time.sleep(1)
 
-        #Uitlezen
-
+        #Read
         df = pd.read_csv(lvm, sep="\t", decimal=",", skiprows=23, usecols=[1, 2, 3], names=["frequency", "gain", "phase"])
         df = df.dropna()
         df = df.replace(",", ".", regex=True).astype(float)
@@ -112,12 +119,14 @@ def myDAQ_sweep(f_start_list:list, f_stop_list:list, steps:list, filename:str, C
 
         print(f"Sweeping range {start_frequency_Hz} to {stop_frequency_Hz} is finished.")
 
+    # This filter is able to filter out the outliers of the measurements.
     f_arr = np.array(f_arr)
     U_gain = np.array(U_gain)
     U_gain_filtered = median_filter(U_gain, size=5)
     U_phase = np.array(U_phase)
     U_phase_filtered = median_filter(U_phase, size=5)
 
+    # This makes from the list a txt-file in the same way txt-files are made by 'National Instruments'
     tijd_datum = datetime.now().strftime("%d-%m-%Y\t%H:%M")
     data = np.column_stack((f_arr, 20 * np.log10(U_gain_filtered), U_phase_filtered))
     header = f"{tijd_datum}\nAmplitude: 2,00 V\nFreq (Hz)\tGain (dB)\tPhase (deg)"
